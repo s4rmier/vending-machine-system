f0a370d1b7a99b5a438ddb66d2f897f0f007a587
hking101 <161253299+hking101@users.noreply.github.com>
2024-11-19 21:11:19 -0800

    Update Transaction.cpp

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,6 +1,51 @@
-#include "Product.cpp"
+#include "Transaction.h"
+#include <iostream>
 
-class Transaction {
-public:
-    Transaction();
-};
+using namespace std;
+
+Transaction::Transaction(int id)
+	:transactionID(id), amountPaid(0.0), changeReturned(0.0),
+	transactionStatus("Pending") {}
+
+bool Transaction::initiateTransaction(float amount) {
+	if (amount <= 0) {
+		cout << "Invalid payment amount. \n";
+		return false;
+	}
+	amountPaid = amount;
+	transactionStatus = "Initiated";
+	return true;
+}
+
+bool Transaction::completeTransaction(float price) {
+	if (amountPaid >= price) {
+		changeReturned = amountPaid - price;
+		transactionStatus = "Completed.";
+		cout << "Transaction completed. Change: $" << changeReturned << "\n";
+		return true;
+	}
+	else {
+		cout << "Insufficient payment. Please add more.\n";
+		return false;
+	}
+}
+
+bool Transaction::refundTransaction() {
+	if (transactionStatus == "Initiated" || transactionStatus == "Completed") {
+		cout << "Transaction refunde. Amount: $" << amountPaid << "\n";
+		transactionStatus = "Refunded";
+		amountPaid = 0.0;
+		return true;
+	}
+	return false;
+}
+
+bool Transaction::cancelTransaction() {
+	if (transactionStatus == "Initiated") {
+		cout << "Transaction canceled.\n";
+			transactionStatus = "Cancelled";
+		amountPaid = 0.0;
+		return true;
+	}
+	return false;
+}
