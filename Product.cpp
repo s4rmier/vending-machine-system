19736b365d9f67843d4ef02de9516e7b55f5ec5f
hking101 <161253299+hking101@users.noreply.github.com>
2024-11-19 18:21:24 -0800

    Update Product.cpp
    product code //hk

--- Product.cpp
+++ Product.cpp
@@ -1,4 +1,20 @@
-class Product {
-public:
-    Product();
-};
+#include "Product.h"
+#include <iostream>
+
+using namespace std;
+
+Product::Product(int id, const string& n, float p, int q)
+	:productID(id), name(n), price(p), quantity(q) {}
+
+int Product::getProductID() const { return productID; }
+string Product::getName() const { return name; }
+float Product::getPrice() const { return price; }
+int Product::getQuantity() const { return quantity; }
+void Product::setQuantity(int q) { quantity = q; }
+
+void Product::displayProduct() const {
+	cout << "Product ID: " << productID
+		<< ", Name: " << name
+		<< ", Price: $" << price
+		<< ", Quantity: " << quantity << "\n";
+}
