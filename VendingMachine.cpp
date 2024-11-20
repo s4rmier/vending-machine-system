30e53249fe0cbed1cde7d236399eec5ed3dfaf6d
hking101 <161253299+hking101@users.noreply.github.com>
2024-11-19 21:15:06 -0800

    Update VendingMachine.cpp
    This is the dummy main I used to call my classes. Right now it is a bit redundant, but it's what I have so far.

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -6,3 +6,47 @@ class VendingMachine {
 public:
     VendingMachine();
 };
+
+
+/* sample main to call my classes - HK
+#include "Product.h"
+#include "Inventory.h"
+#include "Transaction.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//create the products here
+	Product p1(1, "Flaming hot Cheetos", 3.00, 10);
+
+
+	//create the inventory and add products
+	Inventory inventory(101);
+	inventory.addProduct(p1);
+
+	//display inventory
+	inventory.displayInventory();
+
+	//check availability and update inventory
+	if (inventory.checkAvailability(1)) {
+		cout << "Product is available.\n";
+		inventory.updateInventory(1, -1);
+	}
+
+	//display updated inventory
+	inventory.displayInventory();
+
+	//create a transaction
+	Transaction transaction(1001);
+	if (transaction.initiateTransaction(3.00)) {
+		if (transaction.completeTransaction(1.50)) {
+			inventory.updateInventory(1, -1);//reduces the inventory
+		}
+	}
+
+	//refund example
+	transaction.refundTransaction();
+	return 0;
+}
+*/
