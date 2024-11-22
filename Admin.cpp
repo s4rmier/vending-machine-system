bfd2677ad567925aef6e492d12f612806595a7e7
edwardjones3976 <145315321+edwardjones3976@users.noreply.github.com>
2024-11-21 20:00:05 -0800

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,7 +1,28 @@
 #include "Admin.h"
-#include "SalesData.h"
+#include <iostream>
 
-class Admin {
-public:
-    Admin();
-};
+using namespace std;
+
+Admin::Admin(int id, const string& code)
+    : adminID(id), accessCode(code), loginStatus(false) {
+}
+
+bool Admin::login(const string& accessCode) {
+    if (this->accessCode == accessCode) {
+        loginStatus = true;
+        cout << "Login successful.\n";
+        return true;
+    }
+    cout << "Login failed.\n";
+    return false;
+}
+
+SalesData Admin::monitorSalesData() {
+    if (loginStatus) {
+        cout << "Accessing sales data...\n";
+        SalesData salesData;  // Assuming default initialization
+        return salesData;
+    }
+    cout << "Login required to access sales data.\n";
+    return SalesData();  // Return an empty object if not logged in
+}
